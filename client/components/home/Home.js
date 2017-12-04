08f8ac5e48d92facf9bed5ddbde951ef36941296
khmakoto <humbertomakotomorimoto@gmail.com>
2017-12-03 22:12:43 -0600

    Adding instructions of task to landing page.

--- client/components/home/Home.js
+++ client/components/home/Home.js
@@ -28,8 +28,35 @@ class Home extends React.Component {
 		        <Col>
 							<Alert color="warning" isOpen={this.state.visible} toggle={this.onDismiss}>{this.state.alert_message}</Alert>
 							<Jumbotron>
-				        <h1 className="display-3">Hello, Challenger!</h1>
-				        <p className="lead">This is a social annotation challenge.</p>
+								<center>
+									<h1 className="display-4">Welcome to ActivityLab!</h1>
+								</center>
+				        <p className="lead">
+									ActivityLab is a social video annotation challenge in which you can annotate videos based on the activities
+									being performed in them and earn badges for your work.
+								</p>
+								<p className="lead">
+									You can then go ahead and challenge your friends to see who can get the most badges faster! All while
+									contributing to the fostering of research in computer vision!
+								</p>
+								<p className="lead">Below is a set of instructions to give you a sense of what is expected:</p>
+								<ol className="lead">
+									<li>You are to watch a video in which one or more activities are happening.</li>
+									<li>
+										For each of these activities, select the start and end times in which they happen and select the correct
+										label that represents them from the ones presented as most as possible. Then click the "Add" button to add
+										register the specified segment times with the specified label.
+									</li>
+									<li>
+										If you select either the incorrect label or the incorrect segment times, you can delete the segment by
+										clicking on the "Delete" button to the left of the segment you want to delete.
+									</li>
+									<li>
+										Once you have specified all the segments of a video, click the "Submit" button to automatically be taken
+										to the next video.
+									</li>
+									<li>Keep playing so that you can get those achievements!</li>
+								</ol>
 				        <hr className="my-5" />
 				        <p className="lead">
 									<Button outline color="primary" size="lg" tag={Link} to="/task">Go!</Button>
