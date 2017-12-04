38502c5d8a8a61383dfe1ede9c6b5b7e5c4f2a90
Benny Lin <bennylin77@gmail.com>
2017-12-04 01:17:19 +0000

    update

--- server/config/auth.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'facebookAuth' : {
-        'clientID'      : '1957873194239103', // your App ID
-        'clientSecret'  : '8ab82c53718420c2453bf712d60c3b3e', // your App Secret
-        'callbackURL'   : 'http://www.chi-lin.com:7777/auth/facebook/callback',
-        'profileFields' : ['id', 'email', 'name']
-    }
-};
